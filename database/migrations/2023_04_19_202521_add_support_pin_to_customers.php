5c8523415e0d834c28497046c630bf4c56348d04
Grégory BERTINI <gbertini.lpo@gmail.com>
2023-04-20 07:40:45 +0200

    repaired support pin migration

--- database/migrations/2023_04_19_202521_add_support_pin_to_customers.php
+++ database/migrations/2023_04_19_202521_add_support_pin_to_customers.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('customers', function (Blueprint $table) {
-            $table->longText('support_pin');
+            $table->longText('support_pin')->default("00000000");
         });
     }
 
